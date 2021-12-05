3adb626c4fdce35e183cc2632689978b6f8eddc2
Maycol Christian Elcorrobarrutia Medina <Maycol0428@gmail.com>
2021-12-05 18:10:56 -0500

    klasdmfkmdfk

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  //   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
+
+app.listen(port, (req, res) => {
+  console.log("run server");
+});
